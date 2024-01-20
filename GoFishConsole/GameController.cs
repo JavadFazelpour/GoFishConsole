cd52095e47d6e7568105db50fa43a9e3cce511ac
Javad Fazelpour <117605881+JavadFazelpour@users.noreply.github.com>
2024-01-20 15:53:09 +0330

    Added skeleton for GameController class.

--- GoFishConsole/GameController.cs
+++ GoFishConsole/GameController.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace GoFishConsole
 {
-    internal class GameController
+    public class GameController
     {
+        public static Random Random = new Random();
 
+        private GameState gameState;
+        public bool GameOver { get => gameState.GameOver; }
+        public Player HumanPlayer { get => gameState.HumanPlayer; }
+        public IEnumerable<Player> Opponents { get => gameState.Opponents; }
+        public string Status { get; private set; }
+
+        public GameController(string humanPlayerName,IEnumerable<string> computerPlayerNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NextRound(Player playerToAsk,Values valueToAskFor)
+        {
+            throw new NotImplementedException();
+        }
+        private void ComputerPlayersPlayNextRound()
+        {
+            throw new NotImplementedException();
+        }
+        public void NewGame()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
